4c468768e65dec755b243225d2f6811787859c18
goguid <goguid.skunk@gmail.com>
2023-12-20 18:36:06 -0300

    clases abstractas

---
+++ 015-clases-abstractas.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+class Model(ABC):
+    
+    def __init__(self):
+        if not self.tabla:
+            print("Error, tienes que definir una tabla")
+
+    @property
+    @abstractmethod
+    def tabla(self):
+        pass
+
+    def guardar(self):
+        print(f"Guardando {self.tabla} en BBDD")
+
+    @classmethod
+    def buscar_por_id(self, _id):
+        print (f"buscando por id {_id} en la tabla {self.tabla}")
+
+class Usuario(Model):
+    tabla = "Usuario"
+
+usuario = Usuario()
+usuario.guardar()
+Usuario.buscar_por_id(123)
